1247586060b73178917f8214716b9ca08c376a0c
Abhi-s-git <abhi.s.09.2005@gmail.com>
2025-11-08 13:48:10 +0530

    minor changes

--- voice_assist.py
+++ voice_assist.py
@@ -3,7 +3,6 @@ import pyttsx3 as ptx
 import wikipedia as wk
 import pywhatkit as pwk
 import datetime
-import openai as op
 
 r = sr.Recognizer()
 phone_numbers={"abhi":"8884718828","alex":"1234567890","bob":"6382391863816"}
@@ -23,27 +22,29 @@ def commands():
             my_text = r.recognize_google(audioin)
             my_text = my_text.lower()
             print(my_text)
-            #ask to play song
+            
             if 'play' in my_text:
                 my_text = my_text.replace('play','')
                 speak('playing'+my_text)
                 pwk.playonyt(my_text)
-            #speak(my_text)
+            
             elif 'time' in my_text:
                 timenow = datetime.datetime.now().strftime('%H:%M')
+                print(timenow)
                 speak(timenow)
-            #ask anything about person
+            
             elif 'information ' in my_text:
                 person = my_text.replace('information','')
                 info = wk.summary(person,5)
                 print(info)
                 speak(info)
-            #phone number
+            
             elif 'contact details' in my_text:
                 names = list(phone_numbers)
                 for name in names:
                     if name in my_text:
                         speak(name + "Mobile number is " + phone_numbers[name])
+                        print(phone_numbers[name])
             else:
                 speak("sorry,your command not recognized")
                 print("sorry,your command not recognized")
@@ -51,3 +52,4 @@ def commands():
         print("ERROR CAPTURING VOICE...")
 
 commands()
+
